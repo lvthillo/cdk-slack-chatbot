b6357167f2d34135ebb453585bfe1de086fc758a
lvthillo <lorenz.vanthillo@outlook.com>
2022-08-24 19:06:55 +0200

    Run lint

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import { aws_sns as sns, aws_chatbot as chatbot } from 'aws-cdk-lib'
-import { Construct } from 'constructs'
+import { aws_sns as sns, aws_chatbot as chatbot } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 export interface CdkCloudWatchAlarmChatBotProps {
-  readonly topicName: string
-  readonly slackWorkSpaceId: string
-  readonly slackChannelId: string
+  readonly topicName: string;
+  readonly slackWorkSpaceId: string;
+  readonly slackChannelId: string;
 }
 export class CdkCloudWatchAlarmChatBot extends Construct {
-  public readonly topic: sns.Topic
+  public readonly topic: sns.Topic;
   constructor(
     scope: Construct,
     id: string,
-    props: CdkCloudWatchAlarmChatBotProps
+    props: CdkCloudWatchAlarmChatBotProps,
   ) {
-    super(scope, id)
+    super(scope, id);
 
     this.topic = new sns.Topic(this, 'Topic', {
       displayName: props.topicName,
-    })
+    });
 
     new chatbot.SlackChannelConfiguration(this, 'MySlackChannel', {
       slackChannelConfigurationName: 'cw-alarm-slack',
       slackWorkspaceId: props.slackWorkSpaceId,
       slackChannelId: props.slackChannelId,
       notificationTopics: [this.topic],
-    })
+    });
   }
 }
