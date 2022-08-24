ccdff16999a813d31718ddb25253aa4f6aa8971b
Lorenz Vanthillo <lorenz.vanthillo@persgroep.net>
2022-08-24 19:33:15 +0200

    feat: test release.

--- .projenrc.js
+++ .projenrc.js
@@ -1,17 +1,32 @@
 const { awscdk } = require('projen');
+const PROJECT_NAME = 'cdk-cloudwatch-alarm-chatbot';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Lorenz Vanthillo',
   authorAddress: 'lorenz.vanthillo@gmail.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  name: 'cdk-cloudwatch-alarm-chatbot',
-  repositoryUrl: 'https://github.com/lvthillo/cdk-cloudwatch-alarm-chatbot.git',
-
+  depsUpgradeOptions: {
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve'],
+    },
+  },
+  keywords: ['aws', 'cdk', 'chatbot', 'cloudwatch', 'alarm'],
+  majorVersion: 1,
+  name: PROJECT_NAME,
+  publishToPypi: {
+    distName: PROJECT_NAME,
+    module: 'cdk_cloudwatch_alarm_chatbot',
+  },
   deps: [
     'aws-cdk-lib',
   ],
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  stability: 'experimental',
+  repository: 'https://github.com/lvthillo/cdk-cloudwatch-alarm-chatbot.git',
 });
+
+const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'coverage'];
+project.gitignore.exclude(...common_exclude);
+
+project.npmignore.exclude(...common_exclude, 'images');
 project.synth();
