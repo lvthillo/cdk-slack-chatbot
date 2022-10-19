f6c38b2155a503fb91eee5cb9700ea4cde4ff4c4
lvthillo <lorenz.vanthillo@outlook.com>
2022-10-19 12:52:25 +0200

    fix: imports.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { aws_sns as sns, aws_chatbot as chatbot } from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as chatbot from 'aws-cdk-lib/aws-chatbot';
 import { Construct } from 'constructs';
 
 export interface CdkCloudWatchAlarmChatBotProps {
